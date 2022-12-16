e993e8564c0a73f36b4b3b25644c4536f8ad6273
Blagora <santygonza1904@gmail.com>
2022-12-16 14:49:16 -0500

    Listar y traer datos

--- src/main/resources/templates/js/Administrador_js.js
+++ src/main/resources/templates/js/Administrador_js.js
@@ -1,4 +1,4 @@
-var app= {
+var app_prog= {
     backend: "https://jsonplaceholder.typicode.com/users",
     
     leer_prog: function(){      
@@ -8,7 +8,7 @@ var app= {
             "processing": true,
             "paging": false,
             ajax: {
-                url: app.backend,
+                url: app_prog.backend,
                 dataSrc:function(JSON){
                     return JSON;
                 }    
@@ -23,16 +23,28 @@ var app= {
         })
     },
     registrar_prog: function(){
-        $("#reg_programa").click(function(){ 
+        $("#add_programa").click(function(){ 
+            $("#instructores_prog").empty();
             $.ajax({
-                type: "POST",
-                url: "url",
-                data: "data",
-                dataType: "dataType",
-                success: function (response) {
-                    
-                }
+                type: "GET",
+                url: app_prog.backend,
+                dataType: "JSON",
+                error: function(){
+                    alert("Error al importar instructores");
+                },
+            }).done(function(instructores){
+                $.each(instructores, function (i, item) {
+                    $("#reg_instructores_prog").append($('<option>',{
+                        value: item.id,
+                        text: item.name
+                    }));
+                });
             });
+        });
+        $("#reg_programa").click(function () { 
+            $("#reg_prog_ficha").val();
+            $("#reg_prog_prog").val();
+            $("#reg_instructores_prog").val();
         });
     },
     actualizar_prog: function(){
@@ -42,9 +54,36 @@ var app= {
 
     }
 }
-$(document).ready(function () {      
-    app.leer_prog();
-    app.registrar_prog();
-    app.actualizar_prog();
-    app.eliminar_prog();
+app_inst={
+    backend: "https://jsonplaceholder.typicode.com/users",
+
+    leer_inst: function(){      
+        $("#tabla_inst").DataTable({
+            "ordering": false,
+            "info": false,
+            "processing": true,
+            "paging": false,
+            ajax: {
+                url: app_prog.backend,
+                dataSrc:function(JSON){
+                    return JSON;
+                }    
+            },
+            columns:[
+                {data: "id"},
+                {data: "name"},
+                {data: "username"},
+                {data: "address.city"},
+                {defaultContent: "<button type='button' class='btn btn-success' data-toggle='modal' data-target='#editari'>Editar <i class='bi bi-pencil'></i></button>"},
+                {defaultContent: "<button type='button' class='btn btn-danger' >Eliminar <i class='bi bi-trash3'></i></button>"}
+            ]
+        })
+    }
+}
+$(document).ready(function(){      
+    app_prog.leer_prog();
+    app_prog.registrar_prog();
+    app_prog.actualizar_prog();
+    app_prog.eliminar_prog();
+    app_inst.leer_inst();
 });
