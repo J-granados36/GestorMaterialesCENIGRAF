d781afbc0eee147a3ff67c504fa61ad21b9170be
Blagora <santygonza1904@gmail.com>
2022-12-21 18:39:52 -0500

    INSTRUCTORES post

--- src/main/resources/templates/js/Administrador_js.js
+++ src/main/resources/templates/js/Administrador_js.js
@@ -1,20 +1,25 @@
 var app_prog= {
+    // Se llama la url del controller de programas
     backend_prog: "http://localhost:8080/api/ProgramaModel",
     
-    leer_prog: function(){      
+    leer_prog: function(){ 
+        // Se DataTable para darle un diseño por defecto de jquery  
         $("#tabla_prog").DataTable({
+            // Se añadden, editan o quitan elementos de las DataTables
             "ordering": false,
             "info": false,
             "processing": true,
             "paging": false,
             ajax: {
+            // Se usa la url para traer información
                 url: app_prog.backend_prog + "/all",
                 dataSrc:function(JSON){
-                    console.log(JSON);
+                    return JSON;
                 }    
             },
+            // Se añaden las columnas segun los nombres dados en el controlador
             columns:[
-                {data: "id_programa"},
+                {data: "idPrograma"},
                 {data: "nombre_programa"},
                 {data: "username"},
                 {defaultContent: "<button type='button' class='btn btn-success' data-toggle='modal' data-target='#editarp' id='editar_prog'>Editar <i class='bi bi-pencil'></i></button>"},
@@ -130,20 +135,24 @@ var app_prog= {
     }
 }
 app_inst={
+    // Se llama la url del controller de intructores 
     backend: "http://localhost:8080/api/InsModel",
-
+    // Se DataTable para darle un diseño por defecto de jquery 
     leer_inst: function(){      
         $("#tabla_inst").DataTable({
+            // Se añadden, editan o quitan elementos de las DataTables
             "ordering": false,
             "info": false,
             "processing": true,
             "paging": false,
+            // Se usa la url para traer información
             ajax: {
                 url: app_inst.backend + "/all",
                 dataSrc:function(JSON){
                     return JSON;
                 }    
             },
+            // Se añaden las columnas segun los nombres dados en el controlador
             columns:[
                 {data: "cedula"},
                 {data: "nombre"},
@@ -153,6 +162,34 @@ app_inst={
                 {defaultContent: "<button type='button' class='btn btn-danger' >Eliminar <i class='bi bi-trash3'></i></button>"}
             ]
         })
+    },
+    reg_inst:function(){
+        $("#reg_instructores").click(function() { 
+            var reg_instru_docu=$("#reg_instru_docu").val();
+            var reg_instru_nomb=$("#reg_instru_nomb").val();
+            var reg_instru_celu=$("#reg_instru_celu").val();
+            var reg_instru_corr=$("#reg_instru_corr").val();
+            var obj_prog={
+                cedula:reg_instru_docu,
+                celular:reg_instru_celu,
+                correo:reg_instru_corr,
+                nombre_intructor:reg_instru_nomb,
+                nombre:null
+            }
+            $.ajax({
+                type: "POST",
+                url: app_inst.backend + "/save",
+                data: JSON.stringify(obj_prog),
+                contentType: "application/json",
+                success: function (response) {
+                    table.ajax.reload();
+                    table.draw();
+                }
+            }).fail(function($xhr){
+                var data=$xhr.responseJSON;
+            })
+
+        });
     }
 }
 $(document).ready(function(){      
@@ -161,4 +198,5 @@ $(document).ready(function(){
     app_prog.actualizar_prog();
     app_prog.eliminar_prog();
     app_inst.leer_inst();
+    app_inst.reg_inst();
 });
