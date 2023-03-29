311a3bf15ea91d948c04d9c8509b163e93fc88e4
Blagora <santygonza1904@gmail.com>
2023-03-29 14:31:01 -0500

    Nuevo CRUD almacen

--- src/main/java/com/CENIGRAF/GestorMateriales/ControllerWeb/ElementosconsumiblesController.java
+++ src/main/java/com/CENIGRAF/GestorMateriales/ControllerWeb/ElementosconsumiblesController.java
@@ -12,22 +12,21 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping(path = "/productos")
+@RequestMapping(path = "/almacen")
 public class ElementosconsumiblesController {
     @Autowired
     private ElementosconsumiblesRepository productosRepository;
 
     @GetMapping(value = "/agregar")
     public String agregarProducto(Model model) {
-        model.addAttribute("producto", new ElementoConsumible());
-        return "productos/agregar_producto";
+        model.addAttribute("nuevoproducto", new ElementoConsumible());
+        return "almacen/AgregarProducto";
     }
 
-    //http://localhost:8080/productos/mostrar
     @GetMapping(value = "/mostrar")
     public String mostrarProductos(Model model) {
         model.addAttribute("productos", productosRepository.findAll());
-        return "productos/ver_productos";
+        return "almacen/PrincipalAlmacen";
     }
 
     @PostMapping(value = "/eliminar")
@@ -36,7 +35,7 @@ public class ElementosconsumiblesController {
                 .addFlashAttribute("mensaje", "Eliminado correctamente")
                 .addFlashAttribute("clase", "warning");
         productosRepository.deleteById(producto.getId());
-        return "redirect:/productos/mostrar";
+        return "redirect:/almacen/mostrar";
     }
 
     // Se colocó el parámetro ID para eso de los errores, ya sé el id se puede recuperar
@@ -47,9 +46,9 @@ public class ElementosconsumiblesController {
     public String actualizarProducto(@ModelAttribute @Valid ElementoConsumible producto, BindingResult bindingResult, RedirectAttributes redirectAttrs) {
         if (bindingResult.hasErrors()) {
             if (producto.getId() != null) {
-                return "productos/editar_producto";
+                return "/EditarProducto";
             }
-            return "redirect:/productos/mostrar";
+            return "redirect:/almacen/mostrar";
         }
         ElementoConsumible posibleProductoExistente = productosRepository.findFirstByCodigoSena(producto.getCodigoSena());
 
@@ -57,36 +56,36 @@ public class ElementosconsumiblesController {
             redirectAttrs
                     .addFlashAttribute("mensaje", "Ya existe un producto con ese código")
                     .addFlashAttribute("clase", "warning");
-            return "redirect:/productos/agregar";
+            return "redirect:/almacen/agregar";
         }
         productosRepository.save(producto);
         redirectAttrs
                 .addFlashAttribute("mensaje", "Editado correctamente")
                 .addFlashAttribute("clase", "success");
-        return "redirect:/productos/mostrar";
+        return "redirect:/almacen/mostrar";
     }
 
     @GetMapping(value = "/editar/{id}")
     public String mostrarFormularioEditar(@PathVariable int id, Model model) {
         model.addAttribute("producto", productosRepository.findById(id).orElse(null));
-        return "productos/editar_producto";
+        return "/almacen/EditarProducto";
     }
 
     @PostMapping(value = "/agregar")
     public String guardarProducto(@ModelAttribute @Valid ElementoConsumible producto, BindingResult bindingResult, RedirectAttributes redirectAttrs) {
         if (bindingResult.hasErrors()) {
-            return "productos/agregar_producto";
+            return "almacen/AgregarProducto";
         }
         if (productosRepository.findFirstByCodigoSena(producto.getCodigoSena()) != null) {
             redirectAttrs
                     .addFlashAttribute("mensaje", "Ya existe un producto con ese código")
                     .addFlashAttribute("clase", "warning");
-            return "redirect:/productos/agregar";
+            return "redirect:/almacen/agregar";
         }
         productosRepository.save(producto);
         redirectAttrs
                 .addFlashAttribute("mensaje", "Agregado correctamente")
                 .addFlashAttribute("clase", "success");
-        return "redirect:/productos/agregar";
+        return "redirect:/almacen/agregar";
     }
 }
